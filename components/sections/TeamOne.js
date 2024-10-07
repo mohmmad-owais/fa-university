5c2ae9fd229433527e73d25a6700a1ec0f04b99d
mohmmad-owais <precisework01@gmail.com>
2024-10-07 16:26:17 +0500

    added tean members

--- components/sections/TeamOne.js
+++ components/sections/TeamOne.js
@@ -1,62 +1,84 @@
-import React from 'react';
+import React from "react";
 
 const TeamOne = () => {
-    const data = [
-        {
-            img: "team-1.jpg",
-            title: "Edward norton",
-            desig: "Musician"
-        },
-        {
-            img: "team-2.jpg",
-            title: "Jane seymour",
-            desig: "Designer"
-        },
-        {
-            img: "team-3.jpg",
-            title: "Mike hardson",
-            desig: "Developer"
-        },
-        {
-            img: "team-4.jpg",
-            title: "Christine eve",
-            desig: "Artisit"
-        },
-    ];
-    return (
-        <>
-            <section className="team-section">
-                <div className="auto-container">
-                    <div className="sec-title text-center">
-                        <span className="sub-title">qualified teachers</span>
-                        <h2>Meet the teacher who <br />teaches you online</h2>
+  const data = [
+    {
+      img: "team-1.jpg",
+      title: "Edward norton",
+      desig: "Fitness",
+    },
+    {
+      img: "team-2.jpg",
+      title: "Jane seymour",
+      desig: "Lifestyle",
+    },
+    {
+      img: "team-3.jpg",
+      title: "Mike hardson",
+      desig: "Money",
+    },
+    {
+      img: "team-4.jpg",
+      title: "Christine eve",
+      desig: "Relationships",
+    },
+  ];
+  return (
+    <>
+      <section className="team-section">
+        <div className="auto-container">
+          <div className="sec-title text-center">
+            <span className="sub-title">qualified teachers</span>
+            <h2>
+              Meet the teacher who <br />
+              teaches you online
+            </h2>
+          </div>
+          <div className="row">
+            {data.map((item, i) => (
+              <div className="team-block col-xl-3 col-lg-6 col-md-6 col-sm-12 wow fadeInUp">
+                <div className="inner-box">
+                  <div className="image-box">
+                    <figure className="image">
+                      <a href="#">
+                        <img
+                          style={{ objectFit: "cover" }}
+                          className="w-100 h-100"
+                          src={`images/resource/${item.img}`}
+                          title="mentors"
+                        />
+                      </a>
+                    </figure>
+                    <span className="share-icon fa fa-share-alt" />
+                    <div className="social-links">
+                      <a href="#">
+                        <i className="fab fa-twitter" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-facebook-f" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-pinterest-p" />
+                      </a>
+                      <a href="#">
+                        <i className="fab fa-instagram" />
+                      </a>
                     </div>
-                    <div className="row">
-                        {data.map((item, i) => (
-                            <div className="team-block col-xl-3 col-lg-6 col-md-6 col-sm-12 wow fadeInUp">
-                                <div className="inner-box">
-                                    <div className="image-box">
-                                        <figure className="image"><a href="#"><img src={`images/resource/${item.img}`} title="Edulrns" /></a></figure>
-                                        <span className="share-icon fa fa-share-alt" />
-                                        <div className="social-links">
-                                            <a href="#"><i className="fab fa-twitter" /></a>
-                                            <a href="#"><i className="fab fa-facebook-f" /></a>
-                                            <a href="#"><i className="fab fa-pinterest-p" /></a>
-                                            <a href="#"><i className="fab fa-instagram" /></a>
-                                        </div>
-                                    </div>
-                                    <div className="info-box">
-                                        <h4 className="name"><a href="/page-team-details">{item.title}</a></h4>
-                                        <span className="designation">{item.desig}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                  </div>
+                  <div className="info-box">
+                    <h4 className="name">
+                      <a href="/page-team-details">{item.title}</a>
+                    </h4>
+                    <span className="designation">{item.desig}</span>
+                  </div>
                 </div>
-            </section>
-        </>
-    );
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default TeamOne;
